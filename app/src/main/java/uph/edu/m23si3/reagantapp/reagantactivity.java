794abe97f3209ffc6ab2b267f202f2b6610e98a1
ReagantS <03081230050@student.uph.edu>
2025-05-30 18:56:03 +0700

    commit

--- app/src/main/java/uph/edu/m23si3/reagantapp/reagantactivity.java
+++ app/src/main/java/uph/edu/m23si3/reagantapp/reagantactivity.java
@@ -1,24 +1,57 @@
 package uph.edu.m23si3.reagantapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class reagantactivity extends AppCompatActivity {
+
+    EditText inputA, inputB;
+    TextView textHasil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_reagantactivity);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        inputA = findViewById(R.id.inputA);
+        inputB = findViewById(R.id.inputB);
+        textHasil = findViewById(R.id.textHasil);
+
+        Button buttonTambah = findViewById(R.id.buttonTambah);
+        Button buttonKali = findViewById(R.id.buttonKali);
+        Button buttonBagi = findViewById(R.id.buttonBagi);
+
+        buttonTambah.setOnClickListener(v -> hitung("+"));
+        buttonKali.setOnClickListener(v -> hitung("*"));
+        buttonBagi.setOnClickListener(v -> hitung("/"));
+    }
+
+    private void hitung(String operasi) {
+        try {
+            double a = Double.parseDouble(inputA.getText().toString());
+            double b = Double.parseDouble(inputB.getText().toString());
+            double hasil = 0;
+
+            switch (operasi) {
+                case "+":
+                    hasil = a + b;
+                    break;
+                case "*":
+                    hasil = a * b;
+                    break;
+                case "/":
+                    hasil = b != 0 ? a / b : 0;
+                    break;
+            }
+
+            textHasil.setText(String.valueOf(hasil));
+        } catch (NumberFormatException e) {
+            textHasil.setText("Input salah");
+        }
     }
 }
